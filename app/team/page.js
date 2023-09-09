5bc696c529fb6667d69d24d0006d409e9e30d771
Ishmum <ishmum20000@gmail.com>
2023-09-09 21:29:06 +0800

    Load the team members from team data

--- app/team/page.js
+++ app/team/page.js
@@ -1,11 +1,31 @@
 import TeamMember from "../../components/TeamMember/TeamMember";
 import React from "react";
+import teamData from "./teamData";
+import { Box, Grid, Typography } from "@mui/material";
 
 const Team = () => {
   return (
-    <div>
-      <TeamMember />
-    </div>
+    <Box>
+      <Typography variant="h3" fontWeight="bold" align="center">
+        Meet the Team
+      </Typography>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ width: "100%", marginTop: '2em', padding: 10, gap: 7 }}
+      >
+        {teamData().map((member) => (
+          <Grid item>
+            <TeamMember
+              name={member.name}
+              designation={member.designation}
+              imgUrl={member.imgUrl}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
