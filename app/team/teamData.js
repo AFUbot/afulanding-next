05fa6d41489a7c8aaf3923a5e1d691e07bde46cc
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2023-09-10 21:27:49 +0600

    updated scrollbar

--- app/team/teamData.js
+++ app/team/teamData.js
@@ -1,5 +1,4 @@
 const teamData = () => {
-<<<<<<< HEAD
   const teamData = [
     {
       name: "Najmul Islam",
@@ -53,61 +52,6 @@ const teamData = () => {
     },
   ];
   return teamData;
-=======
-    const teamData = [
-        {
-            name: "Najmul Islam",
-            designation: "Founder & Mechanical and System Development",
-            imgUrl: "./team/Najmul.jpeg",
-        },
-        {
-            name: "Tahzeeb Rahman",
-            designation: "Dev Team",
-            imgUrl: "./team/Ishmum.jpg",
-        },
-        {
-            name: "Zuhayer Siddique",
-            designation: "Dev Team",
-            imgUrl: "./team/Zuhayer.jpg",
-        },
-        {
-            name: "Malik Araf",
-            designation: "Dev Team",
-            imgUrl: "./team/Araf.jpeg",
-        },
-        {
-            name: "Abrar Hussain",
-            designation: "Dev Team",
-            imgUrl: "./team/Abrar.png",
-        },
-        {
-            name: "Ruzhan Islam",
-            designation: "Dev Team",
-            imgUrl: "./team/Ruzhan.jpg",
-        },
-        {
-            name: "Shaheed Junaid",
-            designation: "Mechanical and System Development",
-            imgUrl: "./team/Junaid.jpg",
-        },
-        {
-            name: "Fahad Tanvir",
-            designation: "Mechanical and System Development",
-            imgUrl: "",
-        },
-        {
-            name: "Samiur",
-            designation: "Mechanical and System Development",
-            imgUrl: "",
-        },
-        {
-            name: "Anas Amer",
-            designation: "Technical Advisor",
-            imgUrl: "./team/Anas Amer.jpg",
-        },
-    ];
-    return teamData;
->>>>>>> b4270d2e95c0228e9765fecf114259e175b6b5a6
 };
 
 export default teamData;
