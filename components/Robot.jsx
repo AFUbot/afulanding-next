03c648e841db596dbd5b41d65f46138e52d61ed5
Ishmum <ishmum20000@gmail.com>
2023-09-18 04:29:26 +0800

    Revert "Revert "Updated model""
    This reverts commit aeb9d0102cad0462fb8be97bf5ded5b72a8e9c86.

--- components/Robot.jsx
+++ components/Robot.jsx
@@ -11,13 +11,14 @@ import { Box, Typography } from "@mui/material";
 
 const Earth = () => {
   // const robot = useGLTF("./planet/scene.gltf");
-  const robot = useGLTF("./whale/scene.gltf");
+  // const robot = useGLTF("./whale/scene.gltf");
+  const robot = useGLTF("./lowres/untitled.gltf");
 
   return (
     <primitive
       style={{ background: "grey", minHeight: "100vh" }}
       object={robot.scene}
-      scale={0.15}
+      scale={0.03}
       position-y={0}
       rotation-y={0}
     />
@@ -48,7 +49,7 @@ const RobotCanvas = () => {
             minPolarAngle={Math.PI / 2}
           />
           <directionalLight
-            position={[5, 15, 1]} // You can adjust the position of the light
+            position={[3, 3, 20]} // You can adjust the position of the light
             intensity={5} // Adjust the intensity as needed
             castShadow={true} // Enable shadows
           />
