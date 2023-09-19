7685154941fbad56174f8f81eeadbbd973a7c1f3
Ishmum <ishmum20000@gmail.com>
2023-09-20 01:38:09 +0800

    Remove card actions and adjust card size

--- components/TeamMember/TeamMember.jsx
+++ components/TeamMember/TeamMember.jsx
@@ -16,32 +16,32 @@ const TeamMember = (props) => {
     <div>
       <Card
         sx={{
-          width: 350,
+          width: 200,
           background: "black",
           color: "white",
           borderRadius: 5,
         }}
       >
-        <CardMedia sx={{ height: 350 }} image={props.imgUrl} title="" />
+        <CardMedia sx={{ height: 200 }} image={props.imgUrl} title="" />
         <Box
           sx={{ padding: 1, border: 0.5, borderColor: "white", borderTop: 0 }}
         >
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography gutterBottom variant="h6" component="div" sx={{ height: 60 }}>
               {props.name}
             </Typography>
             <Typography variant="body2" color="grey" sx={{ height: 30 }}>
               {props.designation}
             </Typography>
           </CardContent>
-          <CardActions>
+          {/* <CardActions>
             <IconButton aria-label="facebook">
               <Facebook sx={{ color: "white" }} />
             </IconButton>
             <IconButton aria-label="instagram">
               <Instagram sx={{ color: "white" }} />
             </IconButton>
-          </CardActions>
+          </CardActions> */}
         </Box>
       </Card>
     </div>
