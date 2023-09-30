3ffe5ac84a2a766dd4fc6cda9a43db68a6063e35
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2023-09-30 23:44:48 +0600

    navbar fix attempt

--- app/timeline/page.js
+++ app/timeline/page.js
@@ -1,11 +1,9 @@
-"use client";
-
 import Timeline from "@/components/TimelineComp/timelineComp";
 import { Box } from "@mui/material";
 import React from "react";
 
 const TimelinePage = () => {
-  return <Timeline img1={`/timeline/devphase1.jpg`}></Timeline>;
+  return <Timeline></Timeline>;
 };
 
 export default TimelinePage;
